19738cb68d5aefb333f02b68e70f8f43cb9721d0
Anwesh43 <anweshthecool0@gmail.com>
2020-10-09 17:25:38 +0530

    adding TLCNode class

--- index.ts
+++ index.ts
@@ -152,3 +152,43 @@ class Animator {
         }
     }
 }
+
+class TLCNode {
+    next : TLCNode
+    prev : TLCNode 
+    state : State = new State()
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new TLCNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawTLCNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : TLCNode {
+        var curr : TLCNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this 
+    }
+}
