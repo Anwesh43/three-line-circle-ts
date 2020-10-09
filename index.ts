ce62dc869e7348829f92fe05eaa96079c9df3fc9
Anwesh43 <anweshthecool0@gmail.com>
2020-10-09 17:27:19 +0530

    adding THreeLineCircle class

--- index.ts
+++ index.ts
@@ -192,3 +192,26 @@ class TLCNode {
         return this 
     }
 }
+
+class ThreeLineCircle {
+
+    curr : TLCNode = new TLCNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
